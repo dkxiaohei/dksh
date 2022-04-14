5ac334c9e607d021c579f08a3457cdb84119f79a
dkxiaohei <dkxiaohei@outlook.com>
2022-04-14 23:58:50 +0800

    Add '-n' option for 'cat'

--- cat.c
+++ cat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -13,31 +14,37 @@ int cat(int argc, char **args)
 {
     char buf[BUFSIZE];
     int fd, ret;
+    int print_line_number = FALSE, line_number;
     int return_error = FALSE;
 
     // if no files specified, then use STDIN
     if (argc == 1) {
         fd = 0;
-
         while ((ret = read(fd, buf, sizeof(buf))) > 0)
             if (write(1, buf, ret) < 0) {
                 perror("write");
                 return -1;
             }
-    }
-    else {
+    } else {
         while (argc-- > 1) {    // support multiple files
-            fd = open(*++args, O_RDONLY);
+            if (strcmp(*++args, "-n")) {
+                print_line_number = TRUE;
+                continue;
+            } else
+                --args;
+            line_number = 0;
 
+            fd = open(*++args, O_RDONLY);
             if (fd == -1) {
-                fprintf(stderr,
-                        "cat: %s: No such file or directory\n",
-                        *args);
+                fprintf(stderr, "cat: %s: No such file or directory\n", *args);
                 return_error = TRUE;
                 continue;    // try the next file (if any)
             }
 
             while ((ret = read(fd, buf, sizeof(buf))) > 0)
+                if (print_line_number)
+                    printf("%d: ", ++line_number);
+
                 // write to STDOUT
                 if (write(1, buf, ret) < 0) {
                     perror("write");

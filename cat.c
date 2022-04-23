85c550827b4663c7b509e1dbd2dfdc6e5221929f
dkxiaohei <dkxiaohei@outlook.com>
2022-04-23 11:51:05 +0800

    Implement the '-s' option for 'cat'

--- cat.c
+++ cat.c
@@ -7,14 +7,19 @@
 #define BUFSIZE 512
 
 static char buf[BUFSIZE];
-static int print_lineno = FALSE;
-static int print_non_blank_lineno = FALSE;
+static int print_lineno;
+static int print_non_blank_lineno;
+static int squeeze_empty_lines;
+static int is_last_line_empty;
 static int lineno;
 
+static void init();
 static int do_cat(FILE *);
 
 int cat(int argc, char **args)
 {
+    init();
+
     FILE *fp;
     int opt;
     int return_error = FALSE;
@@ -22,13 +27,16 @@ int cat(int argc, char **args)
     opterr = 0;
     optind = 1;
 
-    while ((opt = getopt(argc, args, "nb")) != -1)
+    while ((opt = getopt(argc, args, "nbs")) != -1)
         switch (opt) {
             case 'n':
                 print_lineno = TRUE;
                 break;
             case 'b':
                 print_non_blank_lineno = TRUE;
+                break;
+            case 's':
+                squeeze_empty_lines = TRUE;
                 break;
             default:
                 fprintf(stderr, "Unsupported option: %d\n", opt);
@@ -54,11 +62,27 @@ int cat(int argc, char **args)
     return return_error ? -1 : 0;
 }
 
+static void init()
+{
+    print_lineno = FALSE;
+    print_non_blank_lineno = FALSE;
+    squeeze_empty_lines = FALSE;
+    is_last_line_empty = FALSE;
+}
+
 static int do_cat(FILE *fp)
 {
     lineno = 0;
 
     while ((fgets(buf, sizeof(buf), fp)) != NULL) {
+        if (buf[0] == '\n' && is_last_line_empty && squeeze_empty_lines)
+            continue;
+
+        if (buf[0] == '\n')
+            is_last_line_empty = TRUE;
+        else
+            is_last_line_empty = FALSE;
+
         if (print_non_blank_lineno && buf[0] != '\n')
             printf("%6d\t", ++lineno);
         else if (!print_non_blank_lineno && print_lineno)
@@ -68,6 +92,7 @@ static int do_cat(FILE *fp)
             perror("fputs");
             return -1;
         }
+
         memset(buf, 0, sizeof(buf));
     }
 

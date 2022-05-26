f56ea1380da03eeaef83a2406f910c95ffff75ef
dkxiaohei <dkxiaohei@outlook.com>
2022-05-26 23:08:22 +0800

    Support printing the value of the environment variables in 'echo'

--- echo.c
+++ echo.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int echo(int argc, char **args, int return_value)
 {
+    char *env_var_value;
+
     /* if called with "$?", print the return value of the last command */
-    if (strcmp(args[1], "$?") == 0) {
+    if (args[1] && strcmp(args[1], "$?") == 0) {
         printf("%d\n", return_value);
+        return 0;
+    }
+
+    if (args[1] && args[1][0] == '$') {
+        env_var_value = getenv(&args[1][1]);    /* omit the preceding '$' */
+        printf("%s\n", env_var_value ? env_var_value : "");
         return 0;
     }
 
